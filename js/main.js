1ccd000abf21b664770835b5fc818f104990ef03
LanceChuang <lance8537@gmail.com>
2016-06-02 17:34:02 +0800

    增加about me效果

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
-    $('.box').on('mouseover', function () {
-        $('.box').css('background-color', '#A593E0');
+    $('#box section:nth-child(-n+3)').on('mouseover', function () {
+        $(this).css('background-color', '#A593E0');
     })
     .on('mouseout', function () {
-        $('.box').css('background-color', '');
+        $(this).css('background-color', '');
     });
 });
